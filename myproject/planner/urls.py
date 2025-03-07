20f8948940e367e04642792bc91b738c6e937b74
MoMoJee <2878899607@qq.com>
2025-03-07 16:32:20 +0800

    1.1 开发版 新增了AI自建日程功能，支持交互 升级了AI规划日程功能 通过创建一个包含用户喜好的用户设置数据，其中包含AI配置，允许用户选择AI模型（还没做前端按钮，暂时只支持后台改） 前端接口新增了导出events为ics格式功能，为之后接入手机软件做准备 修改了计划师的逻辑，计划师中AI作出的修改都会保存在temp_events数据中，只有点击应用修改才会合并到events数据
    修复了查看日程详情后无法关闭的Bug
    完善了日程导入功能
    完善了AI交互函数与prompt格式，确保解析正确
    更新kimi的模型为最新版本
    修复了刷新后自动跳到今天的问题（月视图未解决BUG：会往前跳一个月）
    修复了about文档的绝对路径问题

--- myproject/planner/urls.py
+++ myproject/planner/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.planner_index, name='planner_index'),
     path('ai_suggestions/', views.ai_suggestions, name='ai_suggestions'),
+    path('ai_create/', views.ai_create, name='ai_create'),
+    path('get_previous_dialogue/', views.get_previous_dialogue, name='get_previous_dialogue'),
+    path('merge_temp_events/', views.merge_temp_events, name='merge_temp_events'),
+    path('get_temp_long_events/', views.get_temp_long_events, name='get_temp_long_events'),
+
+
 ]
